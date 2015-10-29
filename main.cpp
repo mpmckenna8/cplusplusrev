cba61440fe982b04aa37250cf817935fcb300b4a
mpmckenna8 <mpmckenna8@me.com>
2015-10-29 15:43:47 -0700

    A little start with qread.cpp showing how to read each file which may be a question in the final test.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Final project and review for C++ intro
+// By Matthew McKenna
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    
+
+
+return 0;
+}
---
+++ qread.cpp
@@ -0,0 +1,30 @@
+// Final project and review for C++ intro
+// By Matthew McKenna
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    
+string line;
+
+ifstream fimi;
+
+fimi.open("q1.txt");
+
+if( fimi.is_open() ){
+    while( getline(fimi, line) ) {
+
+        cout << line << " hey" << endl;
+    }
+
+}
+
+fimi.close();
+
+return 0;
+}
