2c8195d7535fad8aadcc605619404034616cc0a3
mpmckenna8 <mpmckenna8@me.com>
2015-12-08 16:09:13 -0800

    Kind of working version but need to jigger it up for lots of questions

--- main.cpp
+++ main.cpp
@@ -2,14 +2,60 @@
 // By Matthew McKenna
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
 
 
 using namespace std;
 
+void printintro();
+
+int quest(int qnum);
+
 int main()
 {
-    
+
+    printintro();
+
+    quest(1);
 
 
 return 0;
+}
+
+
+
+void printintro(){
+
+    cout << "Welcome to the fantasc++al review!" << endl << endl;
+
+}
+
+int quest(int qnum){
+  vector<std::string> question;
+
+    string line;
+
+    ifstream mifi;
+
+    mifi.open("q1.txt");
+
+    if( mifi.is_open() ){
+      int parter= 0;
+      while(getline(mifi, line)){
+    //   mifi >> line;
+      if(line != "88888"){
+        question[parter] += line;
+       cout << line<< endl;
+     }
+     else{
+       parter++;
+     }
+
+    }
+  }
+
+    mifi.close();
+ return 0;
 }
--- readme.md
+++ readme.md
@@ -3,4 +3,43 @@ Final for CS110A C++ at CCSF
 A review of course material for the semester to help get people ready for the
 final or jus to master C++.
 
+Schema for questions is showin in sampQ.txt, outlined below:
 
+Quesiton blah bhal?
+
+888888
+
+answer
+
+888888
+
+wrong
+
+88888
+
+wrong
+
+88888
+
+wrong
+!!!!!!!!!
+
+
+
+For third test know:
+parameter
+- found in the function header
+
+arguments
+- What is sent to the function
+
+return value
+
+function stuff
+
+header
+
+know what happens with & w/ paameters -- making it passing variable not value.
+-  changes var outside function
+
+prototype function
