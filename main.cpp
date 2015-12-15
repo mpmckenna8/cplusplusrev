ad545c2f00ee0c9ed6f828e13d435c73174147c7
mpmckenna8 <mpmckenna8@me.com>
2015-12-15 12:09:08 -0800

    working simple version with a as the answer everytime.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // Final project and review for C++ intro
 // By Matthew McKenna
 
+// including all the necessary libraries so stuff works
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -9,18 +10,44 @@
 
 using namespace std;
 
+// function prototype for printintro function and a function to call for each
+// problem
 bool printintro();
 
+// Will return a 0 if wrong or a 1 if correct.
 int quest(int qnum);
+
+// Good old main function
 
 int main()
 {
-
-    printintro();
     
+    int qNum = 3;
+    int correct = 0;
 
-    quest(1);
+    // Calling a function to print the intro and if the user 
+    if(printintro()){
+        return 0;
+        }
 
+    for(int i = 1; i <= qNum; i++){
+
+        correct += quest(i);
+
+    }
+
+    cout << endl << endl;
+    cout << "test complete!!!!" << endl;
+
+    cout << "Total Number correct is " << correct << " out of " << qNum;
+
+    if(correct == qNum){
+        
+      cout << endl << "you win  go make robots do stuff" << endl;
+    }
+    else{
+      cout << "\n Keep studying till you get em all right" << endl << endl;
+    }
 
 return 0;
 }
@@ -28,7 +55,7 @@ return 0;
 
 
 bool printintro(){
-    
+// intro and menu to do a test or quit
     cout << "Welcome to the fantasc++al review!" << endl << endl;
     char goger = 'B';
 
@@ -37,8 +64,9 @@ bool printintro(){
     cout << "B:) exit \n Make selection and press enter: ";
 
     cin >> goger;
+    cout << endl << endl;
 
-    if(goger == 'A'){
+    if(toupper(goger) == 'A'){
         return false;
     }
     else
@@ -48,23 +76,30 @@ bool printintro(){
 }
 
 int quest(int qnum){
-  vector<std::string> question(20);
+// used a vector which I think gives me credit for using an array to keep track
+// of the parts of each question.
 
+    vector<std::string> question(20);
+// String line to hold each line from the file being read
     string line;
-
+    string finame = "q" + to_string(qnum) + ".txt";
     ifstream mifi;
     int parter= 0;
 // open my file
-    mifi.open("q3.txt");
-
+  //  cout << finame;
+  // Printing out he question number
+  cout << "Q" << qnum << endl;
+    mifi.open(finame);
+// if the file exists and is open do the following.
     if( mifi.is_open() ){
       // get each line and do stuff with it
       while(getline(mifi, line)){
     //   mifi >> line;
       if(line != "88888"){
         question[parter] += line + "\n";
-      //question[parter] = "blah";
-       cout << line<< endl;
+   // some code where I was testing stuff out
+   //question[parter] = "blah";
+      // cout << line<< endl;
      }
      else{
        // increment the part of the array to put stuff in
@@ -75,12 +110,15 @@ int quest(int qnum){
   }
 
     mifi.close();
-    cout << "testing the end \n";
-    cout << question[0] ;
+  //  cout << "testing the end \n";
+  // Print out the question part of the question
+   cout << question[0] ;
+  // Using an array to store the options to the question
     char opts[5] = {'q', 'A', 'B', 'C', 'D'};
 
     int arlen[parter+1];
     string goser[10];
+    // Go through all possible answers to the questions
     for(int i = 1; i <= parter; i++){
       cout << opts[i] << ".)  ";
       cout << question[i];
@@ -89,15 +127,20 @@ int quest(int qnum){
 
     cout << endl << "Answer: ";
     char response;
-
+    // Get user input and store in response variable
     cin >> response;
+    cout << endl << endl;
     response = toupper(response);
+    // Check answer and return 1 if correct, could move to another function,
+    // especially if I want non A answers.
     if(response == 'A'){
-
-      cout << "you win  go make robots do stuff" << endl;
+        return 1;
     }
+    // else return a 0 if the answer is wrong
     else{
-      cout << "Go study this stuff more !!!" << endl;
+    return 0;
     }
- return 0;
+    // return -100 just in case the above stuff didn't work and I'll know there
+    // was a bug
+ return -100;
 }
